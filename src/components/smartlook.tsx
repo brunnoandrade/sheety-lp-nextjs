fbc789c034f5dad56f6263d697c92f374716df55
Tulio Calil <tuliocll@gmail.com>
2025-10-10 22:20:26 -0300

    fix smartlook secret

--- src/components/smartlook.tsx
+++ src/components/smartlook.tsx
@@ -4,9 +4,11 @@ import { useEffect } from 'react';
 import smartlook from 'smartlook-client';
 
 export function SmartlookAnalytics() {
-  useEffect(() => {
-    smartlook.init('SEU_ID_DO_SMARTLOOK');
-  }, []);
+    useEffect(() => {
+        smartlook.init(
+            process.env.NEXT_PUBLIC_SMARTLOOK_ID || ""
+        );
+    }, []);
 
-  return null;
+    return null;
 }
